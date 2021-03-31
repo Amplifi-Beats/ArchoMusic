b0987630cae85b0515b4af8b7df714531c53183d
GianXD <borcillofg2020@gmail.com>
2021-03-31 15:23:43 +0800

    Create ListUtil.java & Add static methods

---
+++ app/src/main/java/com/gianxd/audiodev/util/ListUtil.java
@@ -0,0 +1,48 @@
+package com.gianxd.audiodev.util;
+
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class ListUtil {
+
+    public static void sortArrayList(ArrayList<HashMap<String, Object>> list, String key, boolean isNumber, boolean isAscending) {
+        Collections.sort(list, new Comparator<HashMap<String,Object>>(){
+            public int compare(HashMap<String,Object> compareMap1, HashMap<String,Object> compareMap2){
+                if (isNumber) {
+                    int count1 = Integer.parseInt(compareMap1.get(key).toString());
+                    int count2 = Integer.parseInt(compareMap2.get(key).toString());
+                    if (isAscending) {
+                        return count1 < count2 ? -1 : count1 < count2 ? 1 : 0;
+                    }
+                    else {
+                        return count1 > count2 ? -1 : count1 > count2 ? 1 : 0;
+                    }
+                }
+                else {
+                    if (isAscending) {
+                        return (compareMap1.get(key).toString()).compareTo(compareMap2.get(key).toString());
+                    }
+                    else {
+                        return (compareMap2.get(key).toString()).compareTo(compareMap1.get(key).toString());
+                    }
+                }
+            }
+        });
+    }
+
+    public static ArrayList<HashMap<String, Object>> getArrayListFromSharedJSON(SharedPreferences sharedPreferences, String key) {
+        return new Gson().fromJson(sharedPreferences.getString(key, ""), new TypeToken<ArrayList<HashMap<String, Object>>>(){}.getType());
+    }
+
+    public static HashMap<String, Object> getHashMapFromSharedJSON(SharedPreferences sharedPreferences, String key) {
+        return new Gson().fromJson(sharedPreferences.getString(key, ""), new TypeToken<HashMap<String, Object>>(){}.getType());
+    }
+
+}
--- app/src/main/java/com/gianxd/audiodev/util/MusicDevUtil.java
+++ app/src/main/java/com/gianxd/audiodev/util/MusicDevUtil.java
@@ -14,29 +14,7 @@ import android.view.inputmethod.*;
 
 public class MusicDevUtil {
 
-	public static void sortArrayList(final ArrayList<HashMap<String, Object>> listMap, final String key, final boolean isNumber, final boolean ascending) {
-		Collections.sort(listMap, new Comparator<HashMap<String,Object>>(){
-				public int compare(HashMap<String,Object> _compareMap1, HashMap<String,Object> _compareMap2){
-					if (isNumber) {
-						int _count1 = Integer.valueOf(_compareMap1.get(key).toString());
-						int _count2 = Integer.valueOf(_compareMap2.get(key).toString());
-						if (ascending) {
-							return _count1 < _count2 ? -1 : _count1 < _count2 ? 1 : 0;
-						}
-						else {
-							return _count1 > _count2 ? -1 : _count1 > _count2 ? 1 : 0;
-						}
-					}
-					else {
-						if (ascending) {
-							return (_compareMap1.get(key).toString()).compareTo(_compareMap2.get(key).toString());
-						}
-						else {
-							return (_compareMap2.get(key).toString()).compareTo(_compareMap1.get(key).toString());
-						}
-					}
-				}});
-	}
+
 
 		public static void CropImage(Activity a, String s, int i){
 				try {
