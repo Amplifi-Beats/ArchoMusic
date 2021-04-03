d14c80532c86406c6837705d03a80467fa7879e6
GianXD <borcillofg2020@gmail.com>
2021-04-03 16:08:15 +0800

    Move cropImage() from MusicDevUtil to ImageUtil

--- app/src/main/java/com/gianxd/audiodev/util/ImageUtil.java
+++ app/src/main/java/com/gianxd/audiodev/util/ImageUtil.java
@@ -1,14 +1,39 @@
 package com.gianxd.audiodev.util;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.widget.Toast;
 
 import com.gianxd.audiodev.R;
 
+import java.io.File;
+
 public class ImageUtil {
+
+    public static void cropImage(Activity activity, String path, int result){
+        try {
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            File file = new File(path);
+            Uri contentUri = Uri.fromFile(file);
+            cropIntent.setDataAndType(contentUri, "image/*");
+            cropIntent.putExtra("crop", "true");
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 280);
+            cropIntent.putExtra("outputY", 280);
+            cropIntent.putExtra("return-data", false);
+            activity.startActivityForResult(cropIntent, result);
+        } catch (ActivityNotFoundException anfe) {
+            ApplicationUtil.toast(activity, "Device not supported.", Toast.LENGTH_LONG);
+        }
+    }
 
     public static Bitmap getAlbumArt(String path, Resources resources) {
         String decodedData = "";
