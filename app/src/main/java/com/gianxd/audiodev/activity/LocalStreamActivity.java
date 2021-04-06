10e793202b061502642b5b88d18d52d2ffa3174f
GianXDDDDD <borcillofg2020@gmail.com>
2021-04-06 19:21:37 +0800

    Remove ask current duration when an item is clicked

--- app/src/main/java/com/gianxd/audiodev/activity/LocalStreamActivity.java
+++ app/src/main/java/com/gianxd/audiodev/activity/LocalStreamActivity.java
@@ -1060,21 +1060,6 @@ public class LocalStreamActivity extends  AppCompatActivity  {
 									playbackSrv.createLocalStream(position);
 									profileData.put("lastSongItemPosition", String.valueOf(position));
 									savedData.edit().putString("savedProfileData", ListUtil.setHashMapToSharedJSON(profileData)).apply();
-									if (musicData.get(Integer.parseInt(profileData.get("lastSongItemPosition").toString())).containsKey("lastSongCurrentDuration")) {
-										if (!musicData.get(Integer.parseInt(profileData.get("lastSongItemPosition").toString())).get("lastSongCurrentDuration").equals(playbackSrv.getCurrentPosition())) {
-											Snackbar askContinueDuration = Snackbar.make(miniplayer, "Do you want to skip to the last played duration?", Snackbar.LENGTH_LONG)
-													.setAction("Yes", new View.OnClickListener() {
-														@Override
-														public void onClick(View view) {
-															playbackSrv.seek(Integer.parseInt(musicData.get(position).get("lastSongCurrentDuration").toString()));
-															miniplayerSeekbar.setProgress(Integer.parseInt(musicData.get(position).get("lastSongCurrentDuration").toString()));
-															currentDuration.setText(String.valueOf(((Integer.parseInt(musicData.get(position).get("lastSongCurrentDuration").toString()) / 1000) / 60)).concat(":".concat(new DecimalFormat("00").format((Integer.parseInt(musicData.get(Integer.parseInt(profileData.get("lastSongItemPosition").toString())).get("lastSongCurrentDuration").toString()) / 1000) % 60))));
-															seekbarDuration.setProgress(Integer.parseInt(musicData.get(position).get("lastSongCurrentDuration").toString()));
-														}
-													});
-											askContinueDuration.show();
-										}
-									}
 									playPause.performClick();
 								} catch (Exception e) {
 									ApplicationUtil.toast(getApplicationContext(), "Failed to play selected song. Skipping", Toast.LENGTH_SHORT);
