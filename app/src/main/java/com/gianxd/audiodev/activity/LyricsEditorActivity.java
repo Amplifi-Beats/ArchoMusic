715a2445117f59ed2f78d7c8003fa023be0adcad
GianXDDDDD <borcillofg2020@gmail.com>
2021-04-08 18:14:07 +0800

    Add more dark mode

--- app/src/main/java/com/gianxd/audiodev/activity/LyricsEditorActivity.java
+++ app/src/main/java/com/gianxd/audiodev/activity/LyricsEditorActivity.java
@@ -33,6 +33,7 @@ import static com.gianxd.audiodev.AudioDev.applicationContext;
 public class LyricsEditorActivity extends  AppCompatActivity  { 
 
 	private ArrayList<HashMap<String, Object>> musicData;
+	private HashMap<String, Object> profileData;
 	
 	private LinearLayout toolbar;
 	private EditText lyrics;
@@ -41,31 +42,40 @@ public class LyricsEditorActivity extends  AppCompatActivity  {
 	private ImageView save;
 	
 	private SharedPreferences savedData;
+
 	@Override
-	protected void onCreate(Bundle _savedInstanceState) {
-		super.onCreate(_savedInstanceState);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lyrics_editor);
-		initialize(_savedInstanceState);
+		initialize(savedInstanceState);
 		com.google.firebase.FirebaseApp.initializeApp(this);
 		initializeLogic();
 	}
 	
-	private void initialize(Bundle _savedInstanceState) {
+	private void initialize(Bundle savedInstanceState) {
 		toolbar = (LinearLayout) findViewById(R.id.toolbar);
 		lyrics = (EditText) findViewById(R.id.lyrics);
 		back = (ImageView) findViewById(R.id.back);
 		bruh = (TextView) findViewById(R.id.bruh);
 		save = (ImageView) findViewById(R.id.save);
 		savedData = applicationContext.getSharedPreferences("savedData", Activity.MODE_PRIVATE);
-		musicData = ListUtil.getArrayListFromSharedJSON(savedData, "savedMusicData");
+		if (savedData.contains("savedMusicData")) {
+			musicData = ListUtil.getArrayListFromSharedJSON(savedData, "savedMusicData");
+		} else {
+			musicData = new ArrayList<>();
+		}
+		if (savedData.contains("savedProfileData")) {
+			profileData = ListUtil.getHashMapFromSharedJSON(savedData, "savedProfileData");
+		} else {
+			profileData = new HashMap<>();
+		}
 		lyrics.addTextChangedListener(new TextWatcher() {
 			@Override
 			public void onTextChanged(CharSequence charSequence, int _param2, int _param3, int _param4) {
 				if (lyrics.getText().toString().length() < 1) {
 					save.setEnabled(false);
 					save.setAlpha((float)(0.5d));
-				}
-				else {
+				} else {
 					save.setEnabled(true);
 					save.setAlpha((float)(1.0d));
 				}
@@ -77,7 +87,7 @@ public class LyricsEditorActivity extends  AppCompatActivity  {
 			}
 			
 			@Override
-			public void afterTextChanged(Editable _param1) {
+			public void afterTextChanged(Editable editable) {
 				
 			}
 		});
@@ -111,13 +121,27 @@ public class LyricsEditorActivity extends  AppCompatActivity  {
 	private void initializeLogic() {
 		bruh.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto_medium.ttf"), Typeface.NORMAL);
 		if (Build.VERSION.SDK_INT >= 23) {
-			toolbar.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-			getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
-			getWindow().setNavigationBarColor(Color.parseColor("#EEEEEE"));
-		}
-		else {
+			if (profileData.containsKey("profileDarkMode")) {
+				if (profileData.get("profileDarkMode").equals("true")) {
+					getWindow().setStatusBarColor(Color.parseColor("#1A1A1A"));
+					getWindow().setNavigationBarColor(Color.parseColor("#1A1A1A"));
+				}
+			} else {
+				toolbar.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+				getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
+				getWindow().setNavigationBarColor(Color.parseColor("#FFFFFF"));
+			}
+		} else {
 			getWindow().setStatusBarColor(Color.parseColor("#000000"));
 			getWindow().setNavigationBarColor(Color.parseColor("#000000"));
+		}
+		if (profileData.containsKey("profileDarkMode")) {
+			if (profileData.get("profileDarkMode").equals("true")) {
+				toolbar.setBackgroundColor(Color.parseColor("#1A1A1A"));
+				lyrics.setBackgroundColor(Color.parseColor("#212121"));
+				lyrics.setHintTextColor(Color.parseColor("#BDBDBD"));
+				lyrics.setTextColor(Color.parseColor("#FFFFFF"));
+			}
 		}
 		if (musicData.get((int)Integer.parseInt(getIntent().getStringExtra("songPosition"))).containsKey("songLyrics")) {
 					if (musicData.get((int)Integer.parseInt(getIntent().getStringExtra("songPosition"))).get("songLyrics").toString().length() == 0) {
