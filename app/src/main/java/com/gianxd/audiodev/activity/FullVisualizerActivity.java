3746aad91765d814e7b2860b13312cda1aec56df
GianXDDDDD <borcillofg2020@gmail.com>
2021-04-08 19:28:50 +0800

    Add FullVisualizerActivity.java

---
+++ app/src/main/java/com/gianxd/audiodev/activity/FullVisualizerActivity.java
@@ -0,0 +1,13 @@
+package com.gianxd.audiodev.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullVisualizerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
