67da7b6e39145e3eb0c69b86eb3f2e1799f4e31e
GianXDDDDD <borcillofg2020@gmail.com>
2021-04-13 12:48:14 +0800

    Add some methods

--- app/src/main/java/com/gianxd/audiodev/util/FirebaseUtil.java
+++ app/src/main/java/com/gianxd/audiodev/util/FirebaseUtil.java
@@ -5,8 +5,25 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 import com.gianxd.audiodev.AudioDev;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class FirebaseUtil {
+
+
+    public static void sendMessageToDatabase(String path, String message) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(path);
+        myRef.setValue(message);
+    }
+
+    public static void sendMessageToDatabase(String path, HashMap<String, Object> hashMap) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(path);
+        myRef.setValue(hashMap);
+    }
 
     public static boolean isNetworkConnected() {
         ConnectivityManager connectivityManager = (android.net.ConnectivityManager)AudioDev.applicationContext.getSystemService(Activity.CONNECTIVITY_SERVICE);
