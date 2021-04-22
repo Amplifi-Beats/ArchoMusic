85b66e6dd50a2e8d93ed888ba01619b7aab3f0ea
GianXDDDDD <borcillofg2020@gmail.com>
2021-04-22 20:11:38 +0800

    Add PreferencesActivity

---
+++ app/src/main/java/tk/gianxddddd/audiodev/activity/PreferencesActivity.java
@@ -0,0 +1,14 @@
+package tk.gianxddddd.audiodev.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
