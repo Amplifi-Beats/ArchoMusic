93f2fe8bc11cd18e03e7f1d484a529a2c8be0342
GianXDDDDD <borcillofg2020@gmail.com>
2021-04-29 19:28:18 +0800

    Restore to a recent version of ExternalBrowserActivity

--- app/src/main/java/tk/gianxddddd/audiodev/activity/ExternalBrowserActivity.java
+++ app/src/main/java/tk/gianxddddd/audiodev/activity/ExternalBrowserActivity.java
@@ -1,15 +1,24 @@
 package tk.gianxddddd.audiodev.activity;
 
+import android.content.pm.ActivityInfo;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.RippleDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -20,6 +29,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.FirebaseApp;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import tk.gianxddddd.audiodev.R;
 import tk.gianxddddd.audiodev.util.FileUtil;
@@ -29,10 +39,15 @@ public class ExternalBrowserActivity extends AppCompatActivity {
 
     private HashMap<String, Object> settingsData;
 
+    private WebViewClient webViewClient;
+    private WebChromeClient webChromeClient;
+    private WebSettings webSettings;
+
     private LinearLayout toolbar;
     private ProgressBar loadbar;
     private WebView web;
     private ImageView back;
+    private ImageView options;
     private TextView webtitle;
     private TextView weburl;
 
@@ -47,13 +62,17 @@ public class ExternalBrowserActivity extends AppCompatActivity {
     }
 
     private void initialize() {
-        toolbar = findViewById(R.id.toolbar);
-        loadbar = findViewById(R.id.loadbar);
-        back = findViewById(R.id.back);
-        web = findViewById(R.id.web);
+        toolbar = (LinearLayout) findViewById(R.id.toolbar);
+        loadbar = (ProgressBar) findViewById(R.id.loadbar);
+        back = (ImageView) findViewById(R.id.back);
+        options = (ImageView) findViewById(R.id.options);
+        web = (WebView) findViewById(R.id.web);
+        webSettings = web.getSettings();
 
-        web.getSettings().setJavaScriptEnabled(true);
-        web.getSettings().setSupportZoom(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+        webSettings.setSupportZoom(true);
+        CookieManager.getInstance().setAcceptCookie(true);
 
         webtitle = findViewById(R.id.webtitle);
         weburl = findViewById(R.id.weburl);
@@ -64,34 +83,92 @@ public class ExternalBrowserActivity extends AppCompatActivity {
             settingsData = new HashMap<>();
         }
 
-        web.setWebViewClient(new WebViewClient() {
+        webViewClient = new WebViewClient() {
+
             @Override
             public void onPageStarted(WebView webView, String url, Bitmap favicon) {
-                loadbar.setVisibility(View.VISIBLE);
                 super.onPageStarted(webView, url, favicon);
+
+                loadbar.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onPageFinished(WebView webView, String url) {
+                super.onPageFinished(webView, url);
+
                 loadbar.setVisibility(View.GONE);
-                webtitle.setText(web.getTitle());
+            }
+        };
 
-                if (!url.startsWith("file:///android_asset/")) {
-                    weburl.setText(url);
+        webChromeClient = new WebChromeClient() {
+            private View customView;
+            private WebChromeClient.CustomViewCallback customViewCallback;
 
+            private int originalOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+            private int originalSystemUiVisibility;
+
+            @Override
+            public Bitmap getDefaultVideoPoster() {
+                if (ExternalBrowserActivity.this == null) {
+                    return null;
+                }
+                return BitmapFactory.decodeResource(ExternalBrowserActivity.this.getApplicationContext().getResources(), 2130837573);
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                if (Build.VERSION.SDK_INT >= 24) {
+                    loadbar.setProgress(newProgress, true);
                 } else {
-                    if (url.equals("file:///android_asset/LICENSE.html")) {
-                        weburl.setText("com.gianxd.audiodev/LICENSES");
-                    }
+                    loadbar.setProgress(newProgress);
+                }
+            }
 
-                    if (url.equals("file:///android_asset/PRIVACY.html")) {
-                        weburl.setText("com.gianxd.audiodev/PRIVACY");
-                    }
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+
+                webtitle.setText(title);
+                weburl.setText(view.getUrl());
+            }
+
+            @Override
+            public void onShowCustomView(View view, WebChromeClient.CustomViewCallback viewCallback) {
+                super.onShowCustomView(view, viewCallback);
+
+                if (customView != null) {
+                    onHideCustomView();
+                    return;
                 }
 
-                super.onPageFinished(webView, url);
+                customView = view;
+                originalSystemUiVisibility = ExternalBrowserActivity.this.getWindow().getDecorView().getSystemUiVisibility();
+                ExternalBrowserActivity.this.setRequestedOrientation(originalOrientation);
+                originalOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                customViewCallback = viewCallback;
+                ((FrameLayout) ExternalBrowserActivity.this.getWindow().getDecorView()).addView(customView, new FrameLayout.LayoutParams(-1, -1));
+                ExternalBrowserActivity.this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             }
-        });
+
+            @Override
+            public void onHideCustomView() {
+                super.onHideCustomView();
+
+                ((FrameLayout) ExternalBrowserActivity.this.getWindow().getDecorView()).removeView(customView);
+                customView = null;
+                ExternalBrowserActivity.this.getWindow().getDecorView().setSystemUiVisibility(originalSystemUiVisibility);
+                ExternalBrowserActivity.this.setRequestedOrientation(originalOrientation);
+                originalOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                customViewCallback.onCustomViewHidden();
+                customViewCallback = null;
+            }
+
+        };
+
+        web.setWebViewClient(webViewClient);
+        web.setWebChromeClient(webChromeClient);
 
         back.setOnClickListener(view -> {
             RippleDrawable rippleButton = new RippleDrawable(new ColorStateList(new int[][]{new int[]{}}, new int[]{Color.parseColor("#BDBDBD")}), null, null);
@@ -110,6 +187,7 @@ public class ExternalBrowserActivity extends AppCompatActivity {
 
         loadbar.setBackgroundColor(Color.parseColor("#03A9F4"));
         loadbar.setVisibility(View.GONE);
+        loadbar.setIndeterminate(false);
         loadbar.setElevation(10f);
         toolbar.setElevation(10f);
 
